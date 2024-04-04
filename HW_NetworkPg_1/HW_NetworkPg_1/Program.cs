3e3c7d837225d0d4aab4a46b0313540e5b0dac88
kotlj <crotlj6@gmail.com>
2024-04-04 20:29:26 +0300

    done

---
+++ HW_NetworkPg_1/HW_NetworkPg_1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_NetworkPg_1
+{
+    internal class Program
+    {
+        static string[] CompAnsw = { "Hello!", "How are you?", "Nice!", "Bye" };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter server IP:\n");
+            string servIp = Console.ReadLine();
+            Console.WriteLine("Enter port number:\n");
+            int port = int.Parse(Console.ReadLine());
+
+            try
+            {
+                using (var client = new TcpClient(servIp, port))
+                {
+                    Console.WriteLine("Connected");
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        var reader = new StreamReader(stream, Encoding.UTF8);
+                        var writer = new StreamWriter(stream, Encoding.UTF8) {AutoFlush = true};
+
+                        Console.WriteLine("Choose mode(client - server):\n1 - human - human\n2 - human - computer\n3 - computer - computer\n");
+                        string choise = Console.ReadLine();
+                        string resp = "";
+                        if (choise == "1")
+                        {
+                            writer.WriteLine("1");
+                            Console.WriteLine("Type message(bye for exit):\n");
+                            string userMsg = "";
+                            while ((userMsg = Console.ReadLine()) != null && userMsg.ToLower() != "bye" && resp.ToLower() != "bye")
+                            {
+                                writer.WriteLine(userMsg);
+                                resp = reader.ReadLine();
+                                Console.WriteLine($"get: {resp}");
+                            }
+                        }
+                        else if (choise == "2")
+                        {
+                            writer.WriteLine("2");
+                            Console.WriteLine("Type message(bye for exit):\n");
+                            string userMsg = "";
+                            while ((userMsg = Console.ReadLine()) != null && userMsg.ToLower() != "bye" && resp.ToLower() != "bye")
+                            {
+                                writer.WriteLine(userMsg);
+                                resp = reader.ReadLine();
+                                Console.WriteLine($"get: {resp}");
+                            }
+                        }
+                        else if (choise == "3")
+                        {
+                            writer.WriteLine("2");
+                            string userMsg = "";
+                            Random rand = new Random();
+                            int r = rand.Next(4);
+                            while ((userMsg = CompAnsw[r]) != null && userMsg.ToLower() != "bye" && resp.ToLower() != "bye")
+                            {
+                                writer.WriteLine(userMsg);
+                                resp = reader.ReadLine();
+                                Console.WriteLine($"get: {resp}");
+                                r = rand.Next(4);
+                            }
+                        }
+                        writer.WriteLine("bye");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Disconected");
+        }
+    }
+}
---
+++ HW_TcpServ_1/HW_TcpServ_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_TcpServ_1
+{
+    internal class TCPServer
+    {
+        const int port = 11111;
+        static string[] CompAnsw = { "Hello!", "How are you?", "Nice!", "Bye" };
+        static void Main(string[] args)
+        {
+            var locIp = IPAddress.Any;
+            
+            var server = new TcpListener(locIp, port);
+            server.Start();
+
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Connected");
+
+            NetworkStream stream = client.GetStream();
+            StreamReader sr = new StreamReader(stream, Encoding.UTF8);
+            StreamWriter wr = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+
+            try
+            {
+                string mesg = "";
+                mesg = sr.ReadLine();
+                if (mesg == "2")
+                {
+                    Random rand = new Random();
+                    while ((mesg = sr.ReadLine()) != "bye" && mesg.ToLower() != "bye")
+                    {
+                        int r = rand.Next(4);
+                        Console.WriteLine($"get: {mesg}");
+                        wr.WriteLine($"{CompAnsw[r]}");
+                    }
+                    wr.WriteLine("bye");
+                }
+                else if (mesg == "1")
+                {
+                    string answ = "";
+                    while ((mesg = sr.ReadLine()) != "bye" && mesg.ToLower() != "bye" && answ.ToLower() != "bye")
+                    {
+                        Console.WriteLine($"get: {mesg}");
+                        Console.WriteLine("Enter your answer:\n");
+                        answ = Console.ReadLine();
+                        wr.WriteLine($"{answ}");
+                    }
+                    if (mesg == "bye")
+                    {
+                        wr.WriteLine("bye");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                stream.Close();
+                client.Close();
+            }
+            server.Stop();
+        }
+    }
+}
